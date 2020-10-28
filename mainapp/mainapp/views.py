c8b63794ddbc164cb7893428a8a5acdc84c36ea3
Aslan <baristan1@rambler.ru>
2020-10-28 14:41:12 +0300

    add new view stuff

--- mainapp/mainapp/views.py
+++ mainapp/mainapp/views.py
@@ -1,5 +1,57 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, View
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
+from .mixins import CategoryDetailMixin
 
 
-def test_view(request):
-    return render(request, 'base.html', {})
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page(
+            'notebook', 'smartphone', with_respect_to='notebook'
+        )
+        context = {
+            'categories': categories,
+            'products': products
+        }
+        return render(request, 'base.html', context)
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
+
+    CT_MODEL_MODEL_CLASS = {
+        'notebook': Notebook,
+        'smartphone': Smartphone
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
+        self.queryset = self.model._base_manager.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'product'
+    template_name = 'product_detail.html'
+    slug_url_kwarg = 'slug'
+
+
+class CategoryDetailView(CategoryDetailMixin, DetailView):
+
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
+    slug_url_kwarg = 'slug'
+
+
+class CartView(View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
