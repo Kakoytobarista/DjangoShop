d1a986e1422a7e8ee24a2b6b01d284930674eed7
Aslan <baristan1@rambler.ru>
2020-10-28 14:39:48 +0300

    add new path

--- mainapp/mainapp/urls.py
+++ mainapp/mainapp/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
-from .views import test_view
+from django.urls import path
+from .views import BaseView, ProductDetailView, CategoryDetailView, CartView
 
 
 urlpatterns = [
-    path('', test_view, name='base')
+    path('', BaseView.as_view(), name='base'),
+    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('cart/', CartView.as_view(), name='cart')
 ]
