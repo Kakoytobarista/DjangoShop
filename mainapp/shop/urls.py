37f23a85c29c8044e4f0b0d463388382020a2c4b
Aslan <baristan1@rambler.ru>
2020-10-28 14:29:23 +0300

    add new path

--- mainapp/shop/urls.py
+++ mainapp/shop/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
